ac7aee602e268cc1120ee3caf0b9242d2011a2d6
bartowski20 <nbailey@g.hmc.edu>
2016-06-18 19:51:48 +0000

    working project

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,57 +1,18 @@
 'use strict';
 
-var path = process.cwd();
-var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
-
-module.exports = function (app, passport) {
-
-	function isLoggedIn (req, res, next) {
-		if (req.isAuthenticated()) {
-			return next();
-		} else {
-			res.redirect('/login');
-		}
-	}
-
-	var clickHandler = new ClickHandler();
-
-	app.route('/')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/index.html');
-		});
-
-	app.route('/login')
+var DateHandler = require(process.cwd() + "/app/controllers/dateHandler.js");
+module.exports = function (app) {
+	app.route("/") 
 		.get(function (req, res) {
-			res.sendFile(path + '/public/login.html');
-		});
-
-	app.route('/logout')
+			res.sendFile(process.cwd() + "/public/index.html");
+		});	
+	
+	
+	app.route("/:dateID") 
 		.get(function (req, res) {
-			req.logout();
-			res.redirect('/login');
+			DateHandler(req.params.dateID, function (err, data) {
+				if (err) throw err;
+				res.send(data);
+			});
 		});
-
-	app.route('/profile')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/profile.html');
-		});
-
-	app.route('/api/:id')
-		.get(isLoggedIn, function (req, res) {
-			res.json(req.user.github);
-		});
-
-	app.route('/auth/github')
-		.get(passport.authenticate('github'));
-
-	app.route('/auth/github/callback')
-		.get(passport.authenticate('github', {
-			successRedirect: '/',
-			failureRedirect: '/login'
-		}));
-
-	app.route('/api/:id/clicks')
-		.get(isLoggedIn, clickHandler.getClicks)
-		.post(isLoggedIn, clickHandler.addClick)
-		.delete(isLoggedIn, clickHandler.resetClicks);
 };
