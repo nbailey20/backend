ab5cce35bfbea167a0fb3890e2ad0016937aeab4
bartowski20 <nbailey@g.hmc.edu>
2016-08-04 21:04:53 +0000

    moved controller call

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,7 @@
 
 
 module.exports = function (app) {
-	var DateHandler = require(process.cwd() + "/app/controllers/dateHandler.js");
+//	var DateHandler = require(process.cwd() + "/app/controllers/dateHandler.js");
 	
 	app.route("/home") 
 		.get(function (req, res) {
@@ -12,9 +12,44 @@ module.exports = function (app) {
 	
 	app.route("/:dateID") 
 		.get(function (req, res) {
-			DateHandler(req.params.dateID, function (err, data) {
-				if (err) throw err;
-				res.send(JSON.stringify(data));
-			});
+			//DateHandler(req.params.dateID, function (err, data) {
+			// 	if (err) throw err;
+			// 	res.send(JSON.stringify(data));
+			// });
+			
+			var obj = {unix: "", natural: ""};
+		    var arr = req.params.dateID.split(" ");
+		    var months = ["January", "February", "March", "April", "May", 
+		                            "June", "July", "August", "September", "October",
+		                            "November", "December"];
+		    if (arr.length == 3) {
+		        if (months.indexOf(arr[0]) < 0) {
+		            obj["natural"] = null;
+		            obj["unix"] = null;
+		        }
+		        else if (Number.isNaN(new Date(req.params.dateID).getTime())){
+		            obj["natural"] = null;
+		            obj["unix"] = null;
+		        }
+		        else {
+		            obj["natural"] = req.params.dateID;
+		            var unix = new Date(req.params.dateID).getTime();
+		            obj['unix'] = unix / 1000;
+		        }
+		    }
+		    
+		    else {
+		        obj["unix"] = parseInt(req.params.dateID, 10);
+		        var natural = new Date(+req.params.dateID *1000);
+		        if (natural == "Invalid Date") {
+		            obj['unix'] = null;
+		            obj['natural'] = null;
+		        }
+		        else {
+		            obj['natural'] = months[natural.getMonth()] + " " + natural.getDate() + ", " + natural.getFullYear();
+		        }
+		    }
+		    
+		    res.send(obj);
 		});
 };
--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.use('/public', express.static(process.cwd() + '/public'));
 
 routes(app);
 
-app.listen(process.env.PORT, function () {
+app.listen(8080, function () {
 	console.log("Listening on port 8080...");
 });
